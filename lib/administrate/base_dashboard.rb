de4e3ec4837d844520f4d732c3eb5aa8359f7cbd
kingsleychi <kingsleychi@gmail.com>
2016-05-19 01:29:48 -0400

    Revert "fixed image issue"
    This reverts commit 350454568ed572731e9f7f89b93db561fec42d05.

--- lib/administrate/base_dashboard.rb
+++ lib/administrate/base_dashboard.rb
@@ -1,7 +1,7 @@
 require "administrate/field/belongs_to"
 require "administrate/field/boolean"
 require "administrate/field/date_time"
-#require "administrate/field/email"
+require "administrate/field/email"
 require "administrate/field/has_many"
 require "administrate/field/has_one"
 require "administrate/field/image"
